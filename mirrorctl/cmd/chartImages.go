ec216e095509fdea885ef41fea9caabf8c4aba08
Jose Ortiz <jose.oc@gmail.com>
2025-11-03 21:01:43 +0100

    SBOM - List images used by a helm chart - Refactor

--- mirrorctl/cmd/chartImages.go
+++ mirrorctl/cmd/chartImages.go
@@ -1,19 +1,9 @@
 package cmd
 
 import (
-	"encoding/json"
-	"fmt"
-	"os"
-	"path/filepath"
-	"sort"
-	"strings"
-
-	"github.com/jose-oc/mirror-artifacts/mirrorctl/pkg/sbom/chartscanner"
-	"github.com/jose-oc/mirror-artifacts/mirrorctl/pkg/types"
-	"github.com/rs/zerolog/log"
+	"github.com/jose-oc/mirror-artifacts/mirrorctl/pkg/cmdutils"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"gopkg.in/yaml.v3"
 )
 
 // chartImagesCmd represents the chart-images command
@@ -22,62 +12,7 @@ var chartImagesCmd = &cobra.Command{
 	Short: "List all images used by a Helm chart",
 	Long:  `This command lists all container images referenced within a given Helm chart or a set of charts defined in a YAML file.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		chartsFile := viper.GetString("charts")
-		outputFile := viper.GetString("output-file")
-
-		log.Debug().Msgf("Charts file: %s", chartsFile)
-		if chartsFile == "" {
-			fmt.Println("Error: --charts flag is required")
-			return
-		}
-
-		fmt.Printf("Listing images for charts in: %s\n", chartsFile)
-		imageListByChart, err := chartscanner.ExtractImagesFromCharts(ctx, chartsFile)
-		if err != nil {
-			log.Error().Err(err).Msg("Failed to extract images from charts")
-			return
-		}
-		log.Info().Interface("images", imageListByChart).Msg("Images extracted from charts")
-
-		if outputFile != "" {
-			allImages := make(map[string]types.Image)
-			for _, images := range imageListByChart {
-				for _, image := range images {
-					allImages[image.Source] = image
-				}
-			}
-
-			var sortedImages []types.Image
-			for _, image := range allImages {
-				sortedImages = append(sortedImages, image)
-			}
-
-			sort.Slice(sortedImages, func(i, j int) bool {
-				return sortedImages[i].Source < sortedImages[j].Source
-			})
-
-			imagesList := types.ImagesList{Images: sortedImages}
-
-			var out []byte
-			ext := filepath.Ext(outputFile)
-			if strings.ToLower(ext) == ".json" {
-				out, err = json.MarshalIndent(imagesList, "", "  ")
-			} else {
-				out, err = yaml.Marshal(imagesList)
-			}
-
-			if err != nil {
-				log.Error().Err(err).Msg("Failed to marshal images list")
-				return
-			}
-
-			err = os.WriteFile(outputFile, out, 0644)
-			if err != nil {
-				log.Error().Err(err).Str("file", outputFile).Msg("Failed to write images to file")
-				return
-			}
-			log.Info().Str("file", outputFile).Msg("Wrote images to file")
-		}
+		cmdutils.ExtractImagesFromHelmCharts(ctx, cmd)
 	},
 }
 
--- mirrorctl/pkg/cmdutils/cmdutils.go
+++ mirrorctl/pkg/cmdutils/cmdutils.go
@@ -1,12 +1,22 @@
 package cmdutils
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+
 	"github.com/jose-oc/mirror-artifacts/mirrorctl/pkg/appcontext"
 	"github.com/jose-oc/mirror-artifacts/mirrorctl/pkg/charts"
 	"github.com/jose-oc/mirror-artifacts/mirrorctl/pkg/images"
+	"github.com/jose-oc/mirror-artifacts/mirrorctl/pkg/sbom/chartscanner"
+	"github.com/jose-oc/mirror-artifacts/mirrorctl/pkg/types"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"gopkg.in/yaml.v3"
 )
 
 // MirrorImages handles the `mirror images` subcommand
@@ -34,5 +44,64 @@ func MirrorCharts(ctx *appcontext.AppContext, cmd *cobra.Command) {
 	}
 	if err := charts.MirrorHelmCharts(ctx, chartsFile); err != nil {
 		log.Fatal().Err(err).Msg("Failed to mirror charts")
+	}
+}
+
+func ExtractImagesFromHelmCharts(ctx *appcontext.AppContext, cmd *cobra.Command) {
+	chartsFile := viper.GetString("charts")
+	outputFile := viper.GetString("output-file")
+
+	log.Debug().Msgf("Charts file: %s", chartsFile)
+	if chartsFile == "" {
+		fmt.Println("Error: --charts flag is required")
+		return
+	}
+
+	fmt.Printf("Listing images for charts in: %s\n", chartsFile)
+	imageListByChart, err := chartscanner.ExtractImagesFromCharts(ctx, chartsFile)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to extract images from charts")
+		return
+	}
+	log.Info().Interface("images", imageListByChart).Msg("Images extracted from charts")
+
+	if outputFile != "" {
+		allImages := make(map[string]types.Image)
+		for _, images := range imageListByChart {
+			for _, image := range images {
+				allImages[image.Source] = image
+			}
+		}
+
+		var sortedImages []types.Image
+		for _, image := range allImages {
+			sortedImages = append(sortedImages, image)
+		}
+
+		sort.Slice(sortedImages, func(i, j int) bool {
+			return sortedImages[i].Source < sortedImages[j].Source
+		})
+
+		imagesList := types.ImagesList{Images: sortedImages}
+
+		var out []byte
+		ext := filepath.Ext(outputFile)
+		if strings.ToLower(ext) == ".json" {
+			out, err = json.MarshalIndent(imagesList, "", "  ")
+		} else {
+			out, err = yaml.Marshal(imagesList)
+		}
+
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to marshal images list")
+			return
+		}
+
+		err = os.WriteFile(outputFile, out, 0644)
+		if err != nil {
+			log.Error().Err(err).Str("file", outputFile).Msg("Failed to write images to file")
+			return
+		}
+		log.Info().Str("file", outputFile).Msg("Wrote images to file")
 	}
 }
